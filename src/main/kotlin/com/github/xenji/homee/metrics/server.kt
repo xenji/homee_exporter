1703d4dc8fe54c39389ce9a4564fb3e0461bbf4b
Mario Mueller <mario@xenji.com>
2018-03-10 15:00:06 +0100

    Remove daemon parameter from server call, as it is the default

--- src/main/kotlin/com/github/xenji/homee/metrics/server.kt
+++ src/main/kotlin/com/github/xenji/homee/metrics/server.kt
@@ -2,4 +2,4 @@ package com.github.xenji.homee.metrics
 
 import io.prometheus.client.exporter.HTTPServer
 
-fun startServer(host: String, port: Int) = HTTPServer(host, port, false)
+fun startServer(host: String, port: Int) = HTTPServer(host, port)
