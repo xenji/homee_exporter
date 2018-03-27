749134b4b4504f786dec331b7b6a6a11c0bb6af5
Mario Mueller <mario@xenji.com>
2018-03-27 20:55:27 +0200

    Create generator function for the gauge types to reduce the boilerplate

--- src/main/kotlin/com/github/xenji/homee/metrics/procesing.kt
+++ src/main/kotlin/com/github/xenji/homee/metrics/procesing.kt
@@ -21,68 +21,33 @@ import com.github.xenji.homee.api.Node
 import io.prometheus.client.Gauge
 import java.net.URLDecoder
 
-private val homeeGauge: Gauge = Gauge
-    .build("current_homee", "Tracks the homee status")
-    .labelNames("status_id", "status_name")
-    .register()
-
-private val energyGauge: Gauge = Gauge
-    .build("current_energy", "Tracks the current energy level of a meter")
-    .labelNames("node_id", "attribute_id", "name", "unit")
-    .register()
-
-private val statusGauge = Gauge
-    .build("current_binarystate", "Tracks the current state of a switch")
-    .labelNames("node_id", "attribute_id", "name")
-    .register()
-
-private val motionGauge = Gauge
-    .build("current_motion", "Tracks the current state of a motion detector")
-    .labelNames("node_id", "attribute_id", "name")
-    .register()
-
-private val lightGauge = Gauge
-    .build("current_light", "Tracks the current light level of a sensor")
-    .labelNames("node_id", "attribute_id", "name", "unit")
-    .register()
-
-private val temperatureGauge = Gauge
-    .build("current_temperature", "Tracks the current state of a thermometer")
-    .labelNames("node_id", "attribute_id", "name", "unit")
-    .register()
-
-private val batteryGauge = Gauge
-    .build("current_battery", "Tracks the current battery level")
-    .labelNames("node_id", "attribute_id", "name", "unit")
-    .register()
-
-private val linkQualityGauge = Gauge
-    .build("current_linkquality", "Tracks the current battery level")
-    .labelNames("node_id", "attribute_id", "name", "unit")
-    .register()
+private val homeeGauge: Gauge =
+    Gauge.build("current_homee", "Tracks the homee status").labelNames("status_id", "status_name").register()
+private val energyGauge: Gauge = meteredGauge("current_energy", "Tracks the current energy level of a meter")
+private val statusGauge = binaryGauge("current_binarystate", "Tracks the current state of a switch")
+private val motionGauge = binaryGauge("current_motion", "Tracks the current state of a motion detector")
+private val lightGauge = meteredGauge("current_light", "Tracks the current light level of a sensor")
+private val temperatureGauge = meteredGauge("current_temperature", "Tracks the current state of a thermometer")
+private val batteryGauge = meteredGauge("current_battery", "Tracks the current battery level")
+private val linkQualityGauge = meteredGauge("current_linkquality", "Tracks the current battery level")
 
 fun updateMetrics(nodes: List<Node>, onlyInGroup: Int = -1) = nodes
-    .filter {
-        if (onlyInGroup > 0) it.id in groupToNodeMembership.get(onlyInGroup)
-        else true
-    }
+    .filter { if (onlyInGroup > 0) it.id in groupToNodeMembership.get(onlyInGroup) else true }
     .forEach {
         when (it.id) {
             -1 -> handleHomee(it)
-            else -> it
-                .attributes
-                .forEach { attr ->
-                    when (attr.type) {
-                        1 -> updateHavingBinaryStatus(statusGauge, it.id, it.name, attr)
-                        3, 4 -> updateHavingMeteredValue(energyGauge, it.id, it.name, attr)
-                        5 -> updateHavingMeteredValue(temperatureGauge, it.id, it.name, attr)
-                        8 -> updateHavingMeteredValue(batteryGauge, it.id, it.name, attr)
-                        11 -> updateHavingMeteredValue(lightGauge, it.id, it.name, attr)
-                        14, 19 -> updateHavingBinaryStatus(statusGauge, it.id, it.name, attr)
-                        25 -> updateHavingBinaryStatus(motionGauge, it.id, it.name, attr)
-                        33 -> updateHavingMeteredValue(linkQualityGauge, it.id, it.name, attr)
-                    }
+            else -> it.attributes.forEach { attr ->
+                when (attr.type) {
+                    1 -> updateHavingBinaryStatus(statusGauge, it.id, it.name, attr)
+                    3, 4 -> updateHavingMeteredValue(energyGauge, it.id, it.name, attr)
+                    5 -> updateHavingMeteredValue(temperatureGauge, it.id, it.name, attr)
+                    8 -> updateHavingMeteredValue(batteryGauge, it.id, it.name, attr)
+                    11 -> updateHavingMeteredValue(lightGauge, it.id, it.name, attr)
+                    14, 19 -> updateHavingBinaryStatus(statusGauge, it.id, it.name, attr)
+                    25 -> updateHavingBinaryStatus(motionGauge, it.id, it.name, attr)
+                    33 -> updateHavingMeteredValue(linkQualityGauge, it.id, it.name, attr)
                 }
+            }
         }
     }
 
@@ -92,15 +57,29 @@ private fun handleHomee(node: Node) = when (node.attributes.find { it.type == 20
     2.0 -> homeeGauge.labels("2", "away").set(2.0)
     3.0 -> homeeGauge.labels("3", "vacation").set(3.0)
     else -> {
+        // om-nom-nom
     }
 }
 
 private fun updateHavingBinaryStatus(gauge: Gauge, nodeId: Int, nodeName: String, attr: Attribute) =
-    gauge.labels(nodeId.toString(), attr.id.toString(), nodeName.urlDecoded())
-        .set(attr.current_value)
+    gauge.labels(
+        nodeId.toString(),
+        attr.id.toString(),
+        nodeName.urlDecoded()
+    ).set(attr.current_value)
 
 private fun updateHavingMeteredValue(gauge: Gauge, nodeId: Int, nodeName: String, attr: Attribute) =
-    gauge.labels(nodeId.toString(), attr.id.toString(), nodeName.urlDecoded(), attr.unit.urlDecoded())
-        .set(attr.current_value)
+    gauge.labels(
+        nodeId.toString(),
+        attr.id.toString(),
+        nodeName.urlDecoded(),
+        attr.unit.urlDecoded()
+    ).set(attr.current_value)
+
+private fun meteredGauge(name: String, help: String) =
+    Gauge.build(name, help).labelNames("node_id", "attribute_id", "name", "unit").register()
+
+private fun binaryGauge(name: String, help: String) =
+    Gauge.build(name, help).labelNames("node_id", "attribute_id", "name").register()
 
 fun String.urlDecoded(): String = URLDecoder.decode(this, Charsets.UTF_8.name())
